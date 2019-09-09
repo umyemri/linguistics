ad60b65a62758e8d0cb102cc1eae1bef8ec97b6a
umyemri <40582475+umyemri@users.noreply.github.com>
2019-09-09 14:56:00 -0700

    Update random_syntax.py

--- modules/random_syntax.py
+++ modules/random_syntax.py
@@ -1,26 +1,35 @@
 import random
 
 def stem():
-    stems = ['isuu', 'oj', 'inn', 'sigo', 'jili', 'rao', 'fato', 'joast', \
-        'wui', 'muoi', 'veo', 'och', 'oei', 'vj', 'peii', 'sme' ]
+    stems = ['isuu', 'oj\'', 'inn', 'sigo', 'jili', 'rao', 'fato', 'joast', \
+        'wui', 'muoi', 'veo', 'och\'', 'oei', 'vj', 'peii', 'sme']
     return stems[random.randrange(0, len(stems)-1)]
 
 def root():
     roots = ['bal', 'aop', 'ft', 'ano', 'hji', 'quf', 'cocix', 'jsk', 'jiv', \
-        'voso', 'quoa']
+        'voso', 'quoa', 'iees', ]
     return roots[random.randrange(0, len(roots)-1)]
 
 def particle():
-    parti = ['ur', 'wa', 'to', 'na', 'ha', 'wano']
+    parti = ['ur', 'wa', 'to', 'na', 'ha', 'wano', 'keis', 'sho']
     return parti[random.randrange(0, len(parti)-1)]
 
+def punctuation():
+    punct = ['.', '?', '...', '!', ',']
+    return punct[random.randrange(0, len(punct)-1)]
+
 def main():
-    for i in range(400):
+    for i in range(1000):
         rando = random.randrange(0,100)
-        if rando < 75:
+        if rando <= 75 and rando >= 25:
             print(stem() + root(), end=' ')
-        else:
+        elif rando < 25:
             print(particle(), end=' ')
+            print(stem() + root(), end='')
+            if rando < 10:
+                print(punctuation()+' '+stem().capitalize() + root(), end=' ')
+            else:
+                print(' ', end='')
 
 if __name__ == '__main__':
     main()
